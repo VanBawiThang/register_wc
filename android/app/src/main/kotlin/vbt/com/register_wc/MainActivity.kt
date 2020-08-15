795155ebdf7eb3dab4eb0899c748117724ad8b21
VanBawiThang <41054227+VanBawiThang@users.noreply.github.com>
2020-08-15 22:56:59 +0630

    Initial commit

---
+++ android/app/src/main/kotlin/vbt/com/register_wc/MainActivity.kt
@@ -0,0 +1,6 @@
+package vbt.com.register_wc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+var towns = [
+  "Ahlone",
+  "Kyee Myin Daing",
+  "Bahan",
+  "Pazundaung",
+  "Insein",
+  "Hlaing"
+];
+String _current = towns.elementAt(0);
+
+void main() =>  runApp( MaterialApp(
+  debugShowCheckedModeBanner: false,
+  theme: ThemeData(
+  primarySwatch: Colors.red,
+  visualDensity: VisualDensity.adaptivePlatformDensity,
+),
+home: Scaffold(
+  appBar: AppBar(
+    backgroundColor: Colors.teal,
+    title: Center(
+      child: Text("Register"),
+    ),
+  ),
+  body: Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+    child: Container(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+                labelText: "Name",
+                contentPadding: EdgeInsets.symmetric(horizontal: 10)),
+          ),
+          TextField(
+            decoration: InputDecoration(
+                labelText: "Email",
+                contentPadding: EdgeInsets.symmetric(horizontal: 10)),
+          ),
+          TextField(
+            decoration: InputDecoration(
+                labelText: "Phone",
+                contentPadding: EdgeInsets.symmetric(horizontal: 10)),
+          ),
+          TextField(
+            decoration: InputDecoration(
+                labelText: "Delivery Address",
+                contentPadding: EdgeInsets.symmetric(horizontal: 10)),
+          ),
+          TextField(
+            decoration: InputDecoration(
+                labelText: "Shop Name",
+                contentPadding: EdgeInsets.symmetric(horizontal: 10)),
+          ),
+          // TextField(),
+          Container(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text(
+                  "Postal Code -   ",
+                  style: TextStyle(fontSize: 16, color: Colors.black54),
+                ),
+                DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    style: TextStyle(color: Colors.black),
+                    value: _current,
+                    //isDense: true,
+                    onChanged: (String newValue)  {
+                        _current = newValue;
+                    },
+                    items: towns.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                Text(
+                  "1111111",
+                  style: TextStyle(color: Colors.black54),
+                )
+              ],
+            ),
+          ),
+          Divider(
+            height: 10,
+            color: Colors.black,
+          ),
+          TextField(
+            decoration: InputDecoration(
+                labelText: "Password",
+                contentPadding: EdgeInsets.symmetric(horizontal: 10)),
+          ),
+          TextField(
+            decoration: InputDecoration(
+                labelText: "Confirm Password",
+                contentPadding: EdgeInsets.symmetric(horizontal: 10)),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          FlatButton(
+            color: Colors.teal,
+            child: Text(
+              "SUMMIT",
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () => print("Register Acc"),
+          )
+        ],
+      ),
+    ),
+  ),
+),));
+
+
+
